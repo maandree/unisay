c50b88947b6ece87add6660ae624d42bad9ae387
Mattias Andrée <maandree@operamail.com>
2012-06-22 08:35:36 +0200

    scripts

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm $(find . | grep '~$')       2>  /dev/null
+rm $(find . | grep '.class$')  2>  /dev/null
+rm $(find . | egrep '/(.|)#')  2>  /dev/null
+
---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+## program execution information
+package="se.kth.maandree.unisay"
+main="Unisay"
+
+
+## java executer if default is for Java 7
+[[ $(echo `java -version 2>&1 | cut -d . -f 2` | cut -d ' ' -f 1) = '7' ]] &&
+    function javaSeven()
+    {   java "$@"
+    }
+
+## java executer if default is not for Java 7
+[[ $(echo `java -version 2>&1 | cut -d . -f 2` | cut -d ' ' -f 1) = '7' ]] ||
+    function javaSeven()
+    {   java7 "$@"
+    }
+
+
+## libraries
+jars=''
+if [ -d lib ]; then
+    jars=`echo $(find lib | grep .jar$) | sed -e 's/lib\//:/g' -e 's/ //g'`
+fi
+
+## completion
+if [[ $1 = "--completion--" ]]; then
+    . share/bash-completion/completions/unisay
+    complete -o default -F _unisay run
+
+## run
+else
+    javaSeven -ea -cp bin$jars "$package"."$main" "$@"
+
+fi
