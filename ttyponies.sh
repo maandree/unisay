139632844aaa3147d667d2d9cbb8f4a9cc8c0d5b
Mattias Andrée <maandree@operamail.com>
2012-07-15 01:14:49 +0200

    m

--- ttyponies.sh
+++ ttyponies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for pony in $(ls share/unisay/pony/); do
+for pony in $(ls --color=no share/unisay/pony/); do
     echo "building ttypony: $pony"
-    unisay2ttyunisay < "share/unisay/pony/$pony" | unzebra | tty2colourfultty -c 1 > "share/unisay/ttypony/$pony"
+    unisay2ttyunisay < "share/unisay/pony/$pony" | tty2colourfultty -c 1 > "share/unisay/ttypony/$pony"
 done
 
