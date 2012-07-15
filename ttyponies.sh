a2f8c9ccf20930b317300e2addb7c3813dfc7f1f
Mattias Andrée <maandree@operamail.com>
2012-07-15 15:14:56 +0200

    symlinks

--- ttyponies.sh
+++ ttyponies.sh
@@ -2,6 +2,10 @@
 
 for pony in $(ls --color=no share/unisay/pony/); do
     echo "building ttypony: $pony"
-    unisay2ttyunisay < "share/unisay/pony/$pony" | tty2colourfultty -c 1 > "share/unisay/ttypony/$pony"
+    if [[ `readlink "share/unisay/pony/$pony"` = '' ]]; then
+	unisay2ttyunisay < "share/unisay/pony/$pony" | tty2colourfultty -c 1 > "share/unisay/ttypony/$pony"
+    elif [[ ! -f "share/unisay/ttypony/$pony" ]]; then
+	ln -s `readlink "share/unisay/pony/$pony"` "share/unisay/ttypony/$pony"
+    fi
 done
 
